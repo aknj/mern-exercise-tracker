dab212bee934e403df4c8dd988abdc12a6e95d05
aknj <aknjn@protonmail.com>
2020-08-09 18:15:19 +0200

    set up server & connection to MongoDB Atlas

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+/* configure so that we can have environment variables in a .env file */
+require('dotenv').config();
+
+/* create an express server */
+const app = express();
+const port = process.env.PORT || 5000;
+
+/* setting up middleware */
+app.use(cors());
+app.use(express.json());
+
+/* connect to the MongoDB Atlas database */
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established succesfully.");
+});
+
+/* start the server */
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
