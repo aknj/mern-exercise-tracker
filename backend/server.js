ef5e08bfb311faa042ecb8923714acdcc6c10d92
aknj <aknjn@protonmail.com>
2020-08-09 21:58:50 +0200

    add routes

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,13 @@ connection.once('open', () => {
   console.log("MongoDB database connection established succesfully.");
 });
 
+/* routes */
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
 /* start the server */
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
